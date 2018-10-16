466ee362d79692a27fe90e50ae1d30efddf4a75d
mevaldt <mevaldt@hotmail.com>
2018-10-16 17:39:28 -0300

    correlograma

--- home-credit.R
+++ home-credit.R
@@ -2,6 +2,7 @@
 library(data.table)
 library(tidyverse)
 library(caret)
+library(corrplot)
 
 
 #######  IMPORTING CSV #####
@@ -14,9 +15,22 @@ description <- fread("HomeCredit_columns_description.csv")
 ####### DAYS_EMPLYED CORRECTION #######
 apptrain[, DAYS_EMPLOYED := ifelse(DAYS_EMPLOYED==365243, NA, DAYS_EMPLOYED)]
 
-
 ####### NA IDENTIFICATION #######
 apptrain[, lapply(.SD, function(x) sum(is.na(x)))]
+
+####### NO NA #######
+
+apptrain2 <- na.omit(apptrain)
+nums <- unlist(lapply(apptrain2, is.numeric))  
+
+apptrain_num <- apptrain2[, nums, with=F]
+
+cols_del <- findCorrelation(apptrain_num, cutoff = 0.8)
+
+apptrain_num[, -cols_del, with=F]
+
+correlation <- cor(apptrain_num)
+corrplot.mixed(correlation, tl.cex=1)
 
 View(description)
 
